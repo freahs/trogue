e70fbaae77900c6a2111a72465ce50a7c10e8add
Fredrik Åhs <freahs@kth.se>
2016-03-20 16:49:28 +0100

    cleanup of map class

--- inc/map.hpp
+++ inc/map.hpp
@@ -13,37 +13,37 @@
 #include <random>
 #include <functional>
 
+
+
 namespace trogue {
 
-#define TILE_FLOOR true
-#define TILE_WALL false
+    class MapBuilder;
 
     class Map {
-    protected:
+    private:
         int m_height;
         int m_width;
-        int m_entry_y;
-        int m_entry_x;
 
         std::vector<int>    m_tiles;
-        std::vector<bool>   m_visible_tiles;
+        std::vector<bool>   m_visible;
+        std::vector<bool>   m_walkable;
 
         bool inRange(int y, int x) const;
-        void floodFill(int y, int x);
-
-    protected:
-        void finalize();
-        virtual bool isWall(int y, int x) const = 0;
+        void floodFill();
 
     public:
         Map(int height, int width);
 
-        void set(int row, int col, int i);
-        int get(int row, int col) const;
-
+        void tile(int y, int x, int tile);
+        int tile(int y, int x) const;
+        void visible(int y, int x, bool visible);
+        bool visible(int y, int x) const;
+        void walkable(int y, int x, bool walkable);
+        bool walkable(int y, int x) const;
         int height() const;
         int width() const; 
     };
+
 
     class AutomataMap : public Map {
     private:
@@ -57,26 +57,28 @@ namespace trogue {
 
         std::vector<Param> m_params;
 
-        bool isWall(int y, int x) const override { return true; }
-
     public:
-        using Map::Map;
+        AutomataMap(int height, int width)
+            : Map(height, width) {
+            }
 
         void addStep(int deactivate, int activate, int num_iterations,
-                int neighbour_dist, bool oob_is_wall) { 
-            m_params.push_back({deactivate, activate, num_iterations, neighbour_dist, oob_is_wall});
+                     int neighbour_dist, bool oob_is_wall) { 
+            m_params.push_back({deactivate, activate, num_iterations,
+                               neighbour_dist, oob_is_wall});
         }
 
-        int numAdjecantWalls(int cy, int cx, int distance, bool oob_is_wall) const {
+        int numAdj(int cy, int cx, int distance, bool oob_is_wall,
+                   const std::vector<bool>& walls) const {
             int adj = 0;
             for (int y = -distance; y <= distance; ++y) {
                 for (int x = -distance; x <= distance; ++x) {
                     if (y == 0 && x == 0) { continue; }
                     int _y = cy + y;
                     int _x = cx + x;
-                    if (_y < 0 || _y >= m_height || _x < 0 || _x >= m_width) {
+                    if (_y < 0 || _y >= height() || _x < 0 || _x >= width()) {
                         if (oob_is_wall) { ++adj; }
-                    } else if (get(_y, _x)) {
+                    } else if (walls[_y*width() + _x]) {
                         ++adj;
                     }
                 }
@@ -90,9 +92,13 @@ namespace trogue {
             std::default_random_engine rng(std::random_device{}());
             std::uniform_real_distribution<float> dist(0.0, 1.0);
 
-            for(int y = 1; y < m_height - 1; ++y) {
-                for(int x = 1; x < m_width - 1; ++x) {
-                    set(y, x, wall_prob > dist(rng));
+            std::vector<bool> walls(height()*width(), false); 
+
+            for(int y = 1; y < height() - 1; ++y) {
+                for(int x = 1; x < width() - 1; ++x) {
+                    if (dist(rng) > wall_prob) {
+                        walls[y*width() + x] = true;
+                    }
                 }
             }
 
@@ -100,23 +106,41 @@ namespace trogue {
                 addStep(4, 4, 7, 1, true);
             }
 
+
             for (Param& p : m_params) {
                 for (int i = 0; i < p.num_iterations; ++i) { 
-                    auto tmp_tiles = m_tiles;
-                    for (int y = 1; y < m_height - 1; ++y) {
-                        for (int x = 1; x < m_width - 1; ++x) {
+                    auto tmp_walls(walls);
+                    for (int y = 1; y < height() - 1; ++y) {
+                        for (int x = 1; x < width() - 1; ++x) {
 
-                            int adj = numAdjecantWalls(y, x, p.neighbour_dist, p.oob_is_wall);
+                            int adj = numAdj(y, x, p.neighbour_dist, p.oob_is_wall, walls);
 
-                            if(get(y,x) && adj < p.deactivate)  { tmp_tiles[y * m_height + x] = false; }
-                            else if (get(y,x))                  { tmp_tiles[y * m_height + x] = true; }
-                            else if (adj > p.activate)          { tmp_tiles[y * m_height + x] = true; }
-                            else                                { tmp_tiles[y * m_height + x] = false;  }
+                            if(walls[y*width() + x] && adj < p.deactivate) {
+                                tmp_walls[y * width() + x] = true;
+                            } else if (walls[y*width() + x]) {
+                                tmp_walls[y * width() + x] = false;
+                            } else if (adj > p.activate) {
+                                tmp_walls[y * width() + x] = false;
+                            } else {
+                                tmp_walls[y * width() + x] = true;
+                            }
                         }
                     }
-                    m_tiles = tmp_tiles;
+                    walls = tmp_walls;
                 }
             }
+
+            for (int y = 0; y < height(); ++y) {
+                for (int x = 0; x < width(); ++x) {
+                    bool wall = walls[y*width() + x];
+                    tile(y, x, wall);
+                    walkable(y, x, !wall);
+                    visible(y, x, !wall); 
+                    std::cout << wall;
+                }
+                std::cout << std::endl;
+            }
+
         }
     };
 
--- src/map.cpp
+++ src/map.cpp
@@ -1,5 +1,6 @@
 #include "../inc/map.hpp"
 
+#include <deque>
 #include <set>
 
 namespace trogue {
@@ -7,51 +8,75 @@ namespace trogue {
     Map::Map(int height, int width)
         : m_height(height),
         m_width(width),
-        m_entry_y(-1),
-        m_entry_x(-1),
-        m_tiles(m_height*m_width, false) {
+        m_tiles(m_height*m_width, 0),
+        m_visible(m_height*m_width, false),
+        m_walkable(m_height*m_width, false) {
         }
 
     bool Map::inRange(int y, int x) const {
         return y >= 0 && y < height() && x >= 0 && x < width();
     }
 
-    void Map::floodFill(int start_y, int start_x) {
+    void Map::floodFill() {
         typedef std::pair<int, int> point;
+        std::deque<point> queue;
+        std::vector<bool> visited(height()*width(), false);
 
-        std::set<point> queue;
-        queue.insert({start_y, start_x});
+        for (int i = 0; i < 10; ++i) {
+            for (int j = 0; j < 10; ++j) {
 
-        while (queue.size() > 0) {
-            auto it = queue.begin();
-            int y = it->first;
-            int x = it->second;
-            if (isWall(y,x)) {
-                m_visible_tiles[y*width() + x] = true;
-            } else if (m_visible_tiles[y*width() + x] == false) {
-                m_visible_tiles[y*width() + x] = true;
-                if (inRange(y-1, x-1)) { queue.insert({y-1, x-1}); }
-                if (inRange(y-1, x))   { queue.insert({y-1, x}); }
-                if (inRange(y-1, x+1)) { queue.insert({y-1, x+1}); }
-                if (inRange(y,   x-1)) { queue.insert({y,   x-1}); }
-                if (inRange(y,   x+1)) { queue.insert({y,   x+1}); }
-                if (inRange(y+1, x-1)) { queue.insert({y+1, x-1}); }
-                if (inRange(y+1, x))   { queue.insert({y+1, x}); }
-                if (inRange(y+1, x-1)) { queue.insert({y+1, x+1}); }
+                bool is_first = true;
+                queue.push_back({j,i});
+
+                while (queue.size() > 0) {
+                    int y = queue.front().first;
+                    int x = queue.front().second;
+                    queue.pop_front();
+
+                    if (visible(y, x) && !visited[y*width() + x]) {
+                        visited[y*width() + x] = true;
+                        is_first = false;
+                        if (inRange(y-1, x-1)) { queue.push_back({y-1, x-1}); }
+                        if (inRange(y-1, x))   { queue.push_back({y-1, x}); }
+                        if (inRange(y-1, x+1)) { queue.push_back({y-1, x+1}); }
+                        if (inRange(y,   x-1)) { queue.push_back({y,   x-1}); }
+                        if (inRange(y,   x+1)) { queue.push_back({y,   x+1}); }
+                        if (inRange(y+1, x-1)) { queue.push_back({y+1, x-1}); }
+                        if (inRange(y+1, x))   { queue.push_back({y+1, x}); }
+                        if (inRange(y+1, x-1)) { queue.push_back({y+1, x+1}); }
+                    } else if (!visible(y, x) && !visited[y*width()] && !is_first) {
+                        visited[y*width() + x] = true;
+                        is_first = false;
+                        visible(y, x, true);
+                    }
+                }
             }
-            queue.erase(it);
         }
+
     }
 
-    void Map::finalize() {
+    void Map::tile(int y, int x, int tile) {
+        m_tiles[y*width() + x] = tile;
     }
 
-    void Map::set(int row, int col, int i) {
-        m_tiles[row*width() + col] = i;
+    void Map::visible(int y, int x, bool visible) {
+        m_visible[y*width() + x] = visible;
     }
 
-    int Map::get(int row, int col) const {
-        return m_tiles[row*width() + col];
+    void Map::walkable(int y, int x, bool walkable) {
+        m_walkable[y*width() + x] = walkable;
+    }
+
+    int Map::tile(int y, int x) const {
+        return m_tiles[y*width() + x];
+    }
+
+    bool Map::visible(int y, int x) const {
+        return m_visible[y * width() + x];
+    }
+
+    bool Map::walkable(int y, int x) const {
+        return m_walkable[y * width() + x];
     }
 
     int Map::height() const {
